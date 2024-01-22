df7a2df5386d0e0e3e05731a574115e836f5f56c
Eve_acad <coc123steve@gmail.com>
2024-01-22 17:12:36 +0800

    first commit

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-// Initially hide the video
-document.getElementById('loadingVideo').style.display = 'none';
-
 // Event listener for the "Click to Enter" button
 document.getElementById('enterButton').addEventListener('click', function() {
     var video = document.getElementById('loadingVideo');
+    var loadingScreen = document.getElementById('loadingScreen');
 
     // Unmute the video
     video.muted = false;
@@ -11,6 +9,9 @@ document.getElementById('enterButton').addEventListener('click', function() {
     // Show the video and play it
     video.style.display = 'block';
     video.play();
+
+    // Change the loading screen background to black
+    loadingScreen.style.background = 'black';
 
     // Hide the "Click to Enter" button
     this.style.display = 'none';
@@ -21,4 +22,8 @@ document.getElementById('loadingVideo').addEventListener('ended', function() {
     // Transition to main content when the video ends
     document.getElementById('loadingScreen').style.display = 'none';
     document.getElementById('mainContent').style.display = 'block';
+
+        // Play the background music
+        var backgroundMusic = document.getElementById('backgroundMusic');
+        backgroundMusic.play();
 });
